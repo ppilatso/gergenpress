532d1c737c02f96b3e642ed80b1340cfa478ad8c
ppilatso <pilatsophenyo@gmail.com>
2022-08-31 15:41:49 +0200

    added headlessUI and Optimized navigation for React

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import Link from 'next/link'
-import Styles from './navbar.module.css'
-import Image from 'next/image'
+/* eslint-disable @next/next/no-img-element */
+import React, { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+
+//todo: fix menu button on hover highlight
+//todo: underline nav item on hover
+//todo: appropriate font and logo with optimized Next/Image
 
 const navigation = [
    { name: 'Home', href: '#', current: true },
@@ -16,34 +19,116 @@ const navigation = [
    { name: 'Awards', href: '#', current: false },
    { name: 'FAQ', href: '#', current: false },
    { name: 'Players', href: '#', current: false },
- ]
+]
+
+const myLoader = () => {
+   return `https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80`;
+}
+
+function classNames(...classes: any[]) {
+   return classes.filter(Boolean).join(' ')
+}
 
 function navbar() {
    return (
-      <div>
-         <nav className='bg-slate-50'>
-         <div className="mx-auto items-center max-w-8xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-14 items-center">
-               <div className='px-12'>Bundesliga</div>
-               <div className='flex items-center space-x-2'>
-                  {navigation.map((link, index) => {
-                     return (
-                     <ul className='text-gray-400 px-3 py-2 text-xs font-light' key={index}>
-                        <Link href={link.href}>
-                           <li key={index}>{link.name}</li>
-                        </Link>
-                     </ul>
-                     ); 
-                  })}
-               </div>
+      <Disclosure as='nav' className='bg-white drop-shadow-sm'>
+         {({ open }) => (
+            <>
+            <div className="mx-auto max-w-8xl px-2 sm:px-6 lg:px-36">
+               <div className="relative flex h-16 items-center justify-between">
+                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="flex flex-shrink-0 items-center">
+                           <h2>Bundesliga</h2>
+                        </div>
+                        <div className='hidden mx-auto lg:px-48 sm:ml-6 sm:block'>
+                           <div className='flex space-x-4'>
+                           {navigation.map((item) => (
+                              <a
+                                 key={item.name}
+                                 href={item.href}
+                                 className={classNames(
+                                    item.current ? 'text-gray-500' : 'text-gray-500',
+                                    'px-2 py-2 rounded-md text-xs md:text-sm font-light'
+                                 )}
+                                 aria-current={item.current ? 'page' : undefined}
+                              >
+                                 {item.name}
+                              </a>
+                           ))}
+                        </div>
+                     </div>
+                  </div>
 
-               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                  P
+                  <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+                     <Menu as='div' className='relative ml-3'>
+                        <div>
+                           <Menu.Button 
+                              className='
+                                 flex rounded-full 
+                                 hover:outline-none focus:ring-2 
+                                 hover:ring-white focus:ring-offset-2 
+                                 hover:ring-offset-gray-800'
+                              >
+                              <span className="sr-only">Open user menu</span>
+                              <img
+                                 className="h-8 w-8 rounded-full"
+                                 src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                                 alt='Profile Image'
+                              />
+                           </Menu.Button>
+                        </div>
+                        <Transition
+                           as={Fragment}
+                           enter='transition ease-out duration-100'
+                           enterFrom='transform opacity-0 scale-95'
+                           enterTo='transform opacity-100 scale-100'
+                           leave='transition ease-in duration-75'
+                           leaveFrom='transform opacity-100 scale-100'
+                           leaveTo='transform opacity-0 scale-95'
+                        >
+                           <Menu.Items 
+                              className='absolute right-0 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                              <Menu.Item>
+                                 {({ active }) => (
+                                    
+                                    <a href="#"
+                                       className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                                          Profile
+                                    </a>
+
+                                 )}
+                              </Menu.Item>
+
+                              <Menu.Item>
+                                 {({ active }) => (
+                                    
+                                    <a href="#"
+                                       className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                                          Settings
+                                    </a>
+
+                                 )}
+                              </Menu.Item>
+
+                              <Menu.Item>
+                                 {({ active }) => (
+                                    
+                                    <a href="#"
+                                       className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                                          Sign Out
+                                    </a>
+
+                                 )}
+                              </Menu.Item>
+                           </Menu.Items>
+                        </Transition>
+                     </Menu>
+                  </div>
                </div>
             </div>
-         </div>
-         </nav>
-      </div>
+            </>
+         )}
+      </Disclosure>
    )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className='text-2xl text-red-600'>Fantasy League - Preview!</div>
+    <div className='bg-slate-100 text-2xl text-red-600'>Fantasy League - Preview!</div>
   )
 }
 
